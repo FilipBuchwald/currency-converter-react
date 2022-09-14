7d4b7078dd67f92739584e3c5b7f930c2d3c9699
Filip Buchwald <fixu1998@gmail.com>
2022-09-14 19:35:45 +0200

    added calculate result

--- src/App.js
+++ src/App.js
@@ -2,14 +2,30 @@ import Form from "./Form";
 import Header from "./Header";
 import Footer from "./Footer";
 import Container from "./Container";
+import { useState } from "react";
+import { currencyBase } from "./Form/CurrencyBase";
 
 function App() {
-  return (
+  const [result, setResult] = useState();
 
+  const calculateResult = (currency, amount) => {
+    const rate = currencyBase.find(({ name }) => name === currency).rate;
+
+    setResult({
+      targetAmount: amount / rate,
+      currency,
+    });
+  };
+
+
+  return (
     <Container>
-      <Header title ="Kalkulator Walut" />
-      <Form />
-      <Footer title="Kurs z dnia ..."/>
+      <Header title="Kalkulator Walut" />
+      <Form
+        calculateResult={calculateResult}
+        result={result}
+      />
+      <Footer title="Kurs z dnia 13.09.2022" />
     </Container>
 
   );
