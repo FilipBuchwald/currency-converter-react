4e2555578664853f3d796c65fd1227f0b817e0e5
Filip Buchwald <fixu1998@gmail.com>
2022-10-19 10:33:25 +0200

    used async/await in useRateData

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,76 +1,77 @@
-import { useRatesData } from "./useRateData";
 import { useState } from "react";
+import { useRatesData } from "./useRateData";
 import { Result } from "./Result";
 import { StyledForm, Fieldset, Legend, Input, Select, Button } from "./styled";
 
 const Form = () => {
-    const onFormSubmit = (event) => {
-        event.preventDefault();
-        calculateResult(currency, amount);
-    };
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+    calculateResult(currency, amount);
+  };
 
-    const [amount, setAmount] = useState("");
-    const [currency, setCurrency] = useState("USD");
-    const onSelectChange = ({ target }) => setCurrency(target.value);
-    
-    
-    const ratesData = useRatesData();
+  const [amount, setAmount] = useState("");
+  const [currency, setCurrency] = useState("USD");
+  const onSelectChange = ({ target }) => setCurrency(target.value);
 
-    const [result, setResult] = useState();
 
-    const calculateResult = (currency, amount) => {
-        const rate = ratesData[currency];
+  const ratesData = useRatesData();
+  console.log(ratesData);
 
-        setResult({
-        targetAmount: amount * rate,
-        currency,
-        });
-    };
-    
-    return (
-        <StyledForm
-            onSubmit={onFormSubmit}
-        >
-            <Fieldset>
-                <Legend>Kalkulator</Legend>
-                <div>
-                    <label>
-                        <span>
-                            Kwota w zł:
-                        </span>
-                        <Input
-                            value={amount}
-                            onChange={({ target }) => setAmount(target.value)}
-                            placeholder="Wpisz kwotę"
-                            required
-                            autoFocus
-                        />
-                    </label>
-                </div>
+  const [result, setResult] = useState();
 
-                <div>
-                    <label>
-                        Chcę otrzymać:
-                        <Select
-                            value={currency}
-                            onChange={onSelectChange}
-                        >
-                            {Object.keys(ratesData).map((currency => (
-                                <option
-                                    key={currency}
-                                    value={currency}
-                                >
-                                    {currency}
-                                </option>
-                            )))}
-                        </Select>
-                    </label>
-                </div>
-                <Button className="form__button ">Przelicz</Button>
-                <Result result={result} />
-            </Fieldset>
-        </StyledForm>
-    );
+  const calculateResult = (currency, amount) => {
+    const rate = ratesData.rates[currency];
+
+    setResult({
+      targetAmount: amount * rate,
+      currency,
+    });
+  };
+
+  return (
+    <StyledForm
+      onSubmit={onFormSubmit}
+    >
+      <Fieldset>
+        <Legend>Kalkulator</Legend>
+        <div>
+          <label>
+            <span>
+              Kwota w zł:
+            </span>
+            <Input
+              value={amount}
+              onChange={({ target }) => setAmount(target.value)}
+              placeholder="Wpisz kwotę"
+              required
+              autoFocus
+            />
+          </label>
+        </div>
+
+        <div>
+          <label>
+            Chcę otrzymać:
+            <Select
+              value={currency}
+              onChange={onSelectChange}
+            >
+              {ratesData.rates && Object.keys(ratesData.rates).map((currency => (
+                <option
+                  key={currency}
+                  value={currency}
+                >
+                  {currency}
+                </option>
+              )))}
+            </Select>
+          </label>
+        </div>
+        <Button className="form__button ">Przelicz</Button>
+        <Result result={result} />
+      </Fieldset>
+    </StyledForm>
+  );
 };
 
 export default Form;
--- src/Form/useRateData.js
+++ src/Form/useRateData.js
@@ -2,15 +2,42 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 
 export const useRatesData = () => {
-
-    const [rates, setRates] = useState([]);
-  
+    const [rates, setRates] = useState(
+        {state: "loading"}
+        // []
+        );
+   
     useEffect(() => {
-        axios.get("https://api.exchangerate.host/latest?base=PLN")
-        .then(response => {
-            setRates(response.data.rates);
-        })
-    },[]);
+        const requestRateData = async () => {
+            try {
+                const response = await axios.get("https://api.exchangerate.host/latest?base=PLN");
+                
+                console.log(response);
+                
+                const { rates, date } =  response.data
     
+                setRates({
+                    rates,
+                    date,
+                    state: "success",
+                });
+
+            } 
+            catch (error) {
+                setRates({
+                    state: "error",
+                });
+                console.log(error);
+            }
+        };
+        // axios.get("https://api.exchangerate.host/latest?base=PLN")
+        // .then(response => {
+        //     setRates(response.data.rates);
+
+
+        setTimeout(requestRateData, 2000);
+
+    }, []);
+
     return rates;
 };
