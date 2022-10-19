ae7432025211a75229ba97671cf3b0e8ed78f154
Filip Buchwald <fixu1998@gmail.com>
2022-10-19 12:34:16 +0200

    removed footer component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import Form from "./Form";
 import Header from "./Header";
-import Footer from "./Footer";
 import Container from "./Container";
 
 function App() {
@@ -9,7 +8,6 @@ function App() {
     <Container>
       <Header title="Kalkulator Walut" />
       <Form/>
-      <Footer title="Kurs z dnia 13.09.2022" />
     </Container>
   );
 };
--- src/Footer/index.js
+++
@@ -1,9 +0,0 @@
-import { StyledFooter } from "./styled";
-
-const Footer = ({ title }) => (
-    <StyledFooter>
-        <p>{title}</p>
-    </StyledFooter>
-);
-
-export default Footer
